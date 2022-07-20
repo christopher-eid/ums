9975d4a759547030a823190432d95e251ac0490a
christopher <christopher.c.eid@outlook.com>
2022-07-20 16:44:30 +0300

    implement signup for users

---
+++ UMS.Application/User/Commands/SignUp/SignUpCommand.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Application.Models;
+using MediatR;
+
+namespace Application.User.Commands.SignUp;
+
+public class SignUpCommand: IRequest<SignUpReturnInfoDto>
+{
+    [Required(ErrorMessage = "Name field is required")]
+    public string? Name { get; set; }
+    
+    [Required(ErrorMessage = "Email field is required")]
+    public string? Email { get; set; }
+    
+    [Required(ErrorMessage = "Role ID is required")]
+    public long RoleId { get; set; }
+}
